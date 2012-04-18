35abdb7412b0930d90d82c4841300738bb22b514
Isak Sky <isak.sky@gmail.com>
2012-04-17 20:45:37 -0700

    Added combinatorics dependency

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject cmu-katas "0.0.1"
   :description "Katas for the CMU Software Craftmanship course"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/math.combinatorics "0.0.2"]])
