f5a6b2b452c14c3afad68ce4eeb7e153f50480d4
Isak Sky <isak.sky@gmail.com>
2012-04-30 13:42:35 -0700

    Did the unbeatable tic-tac-toe kata.

---
+++ src/cmu_katas/tic_tac_toe.clj
@@ -0,0 +1,123 @@
+(ns cmu-katas.tic-tac-toe)
+
+;; Excercise: Create unbeatable tic-tac-toe
+
+;; Player representation :a or :b
+
+;; Board representation
+;; Hash with coords  => :a or :b
+;; E.g., {[0 0] :a [1 0] :b  [2 0] :a}
+
+(def horizontal-coord-lists
+  (for [y (range 0 3)]
+    (for [x (range 0 3)]
+      [x y])))
+
+(def vertical-coord-lists
+  (for [x (range 0 3)]
+    (for [y (range 0 3)]
+      [x y])))
+
+(def diagonal-coord-lists
+  [[[0 0] [1 1] [2 2]]
+   [[0 2] [1 1] [2 0]]])
+
+(def all-winning-coord-lists
+  (concat horizontal-coord-lists
+          vertical-coord-lists
+          diagonal-coord-lists))
+
+(defn player-frequencies [board coord-list]
+  (let [freqs (->> coord-list
+                   (map #(board %))
+                   frequencies)]
+    {:a (or (freqs :a) 0)
+     :b (or (freqs :b) 0)}))
+
+(def all-coords
+  (for [x (range 3) y (range 3)]
+    [x y]))
+
+(defn empty-coords [board]
+  (remove #(board %) all-coords))
+
+(defn game-result
+  "Returns :a if player A won, :b if player B won, or nil if no winner."
+  [board]
+  (->> all-winning-coord-lists
+       (map #(player-frequencies board %))
+       (map (fn [player-frequency]
+              (cond (= 3 (:a player-frequency)) :a
+                    (= 3 (:b player-frequency)) :b)))
+       (filter identity)
+       first))
+
+(defn game-finished? [board]
+  (or (= 0 (count (empty-coords board)))
+      (game-result board)))
+
+(defn other-player [player]
+  (if (= player :a) :b :a))
+
+(defn blocked? [board coord-list player]
+  (let [other-player (other-player player)
+        other-player-freqs (other-player (player-frequencies board coord-list))]
+    (and other-player-freqs (< 0 other-player-freqs))))
+
+(defn blocking-score [board player coord-list coords]
+  (let [player-frequencies (player-frequencies board coord-list)
+        other-player (other-player player)]
+    (cond
+     ;; No points if other player hasn't started on path
+     (= 0 (player-frequencies other-player)) 0
+     ;; No points if player is already blocked
+     (blocked? board coord-list other-player) 0
+     ;; Take exponent, because danger does not scale linearly with how many they have
+     :else (int (Math/pow (player-frequencies other-player) 3)))))
+
+(defn win-progress-score [board player coord-list coords]
+  (cond (blocked? board coord-list player) 0
+        :else (let [player-frequencies (player-frequencies board coord-list)]
+                ;;Grows faster than block, b/c would rather win than block
+                (int (Math/pow (player-frequencies player) 5)))))
+
+(defn score [board coords player]
+  (let [related-coord-lists (filter #(some #{coords} %)
+                                    all-winning-coord-lists)]
+    (reduce + (map (fn [scoring-function]
+                     (reduce + (map (fn [coord-list] (scoring-function board player coord-list coords))
+                                    related-coord-lists)))
+                   [blocking-score win-progress-score]))))
+
+(defn best-move-coords [board player]
+  (->> (empty-coords board)
+       (sort-by (fn [coords] (score board coords player)))
+       last))
+
+(defn move [board player]
+  (cond (empty? board) (assoc board [1 1] player)
+        (game-finished? board) nil
+        :else (let [best-move-coords (best-move-coords board player)]
+                (assoc board best-move-coords player))))
+
+(defn rand-move [board player]
+  (if-let [empty-coords (empty-coords board)]
+    (assoc board (rand-nth empty-coords) player)))
+
+(defn print-board [board]
+  (doseq [y (range 3) x (range 3)]
+    (if (= 0 x) (print "\n"))
+    (print (or (board [x y]) ":_"))))
+
+(defn print-sample-game [against-self?]
+  (loop [board {}
+         player-turns (cycle [[:a move] [:b (if against-self?
+                                              move
+                                              rand-move)]])
+         all-boards []]
+    (cond (game-finished? board) (do (doseq [board all-boards]
+                                       (do (print-board board) (println)))
+                                     (println "\nWinner: " (game-result board)))
+          :else (let [[player move-fn] (first player-turns)
+                      new-board (move-fn board player)]
+                  (recur new-board (rest player-turns) (conj all-boards new-board))))))
---
+++ test/cmu_katas/test/tic_tac_toe.clj
@@ -0,0 +1,88 @@
+(ns cmu-katas.test.tic-tac-toe
+  (:use [clojure.test])
+  (:use [cmu-katas.tic-tac-toe]))
+
+(deftest player-frequencies-test
+  (is (= {:a 2 :b 1} (player-frequencies {[0 0] :a [1 1] :a [2 2] :b} [[0 0] [1 1] [2 2]]))))
+
+(def tied-board
+  {[0 0] :a [1 0] :b [2 0] :b
+   [0 1] :b [1 1] :a [2 1] :a
+   [0 2] :a [1 2] :b [2 2] :a})
+
+(def unfinished-board
+  (dissoc tied-board [1 1]))
+
+(deftest game-finished?-test
+  (is (game-finished? tied-board))
+  (is (not (game-finished? unfinished-board))))
+
+(deftest blocked?-test
+  (is (blocked? {[0 0] :a [1 0] :b} (first horizontal-coord-lists) :a))
+  (is (not (blocked? {[0 0] :a [1 0] :a} (first horizontal-coord-lists) :a))))
+
+(deftest game-result-test
+  (is :a (game-result {[0 0] :a [1 0] :a [2 0] :a}))
+  (is :b (game-result {[0 0] :b [1 1] :b [2 2] :b}))
+  (is nil? (game-result {[0 0] :a [1 0] :a [2 0] :b})))
+
+(deftest empty-coords-test
+  (is (= [[1 1]] (empty-coords unfinished-board))))
+
+(def partial-board-1
+  {[0 0] :b [1 0] :a})
+
+(def top-horizonal-coord-list
+  [[0 0] [1 0] [2 0]])
+
+(def partial-board-2
+  {[0 0] :b })
+
+(def partial-board-3
+  {[0 0] :b [1 0] :b})
+
+(deftest blocking-score-test
+  (is (= 0 (blocking-score partial-board-1 :a top-horizonal-coord-list [2 0])))
+  (is (= 1 (blocking-score partial-board-2 :a top-horizonal-coord-list [2 0])))
+  (is (= 8 (blocking-score partial-board-3 :a top-horizonal-coord-list [2 0]))))
+
+(deftest win-progress-score-test
+  (is (= 0 (win-progress-score {} :a top-horizonal-coord-list [2 0])))
+  (is (= 1 (win-progress-score partial-board-2 :b top-horizonal-coord-list [2 0])))
+  (is (= 32 (win-progress-score partial-board-3 :b top-horizonal-coord-list [2 0]))))
+
+(def partial-board-4
+  {[0 0] :a [1 0] :a
+   [2 1] :b
+   [2 2] :b})
+
+(deftest score-test
+  (is (= 40 (score partial-board-4 [2 0] :a))))
+
+(def partial-board-5
+  {[0 0] :b [1 0] :b
+   [1 1] :a})
+
+(deftest best-move-coords-test
+  (is (= [2 0] (best-move-coords partial-board-3 :b)))
+  (is (= [2 0] (best-move-coords partial-board-5 :a))))
+
+(deftest never-lose-test
+  (let [rand-game-results
+        (for [i (range 999)]
+          (loop [board {}
+                 player-turns (cycle [[:a move] [:b rand-move]])
+                 all-boards []]
+            (cond (game-finished? board)
+                  (let [game-result (game-result board)]
+                    (if (= :b game-result)
+                      (do (println "PROGRAM FAIL! LEARN TO PROGRAM BETTER!")
+                          (doseq [board all-boards]
+                            (do (print-board board)
+                                (println)))))
+                    game-result)
+                  :else (let [[player move-fn] (first player-turns)
+                              new-board (move-fn board player)]
+                          (recur new-board (rest player-turns) (conj all-boards new-board))))))]
+    ;; Game can be tie, but player B can never win
+    (is (= 0 (or (:b (frequencies rand-game-results)) 0)))))
